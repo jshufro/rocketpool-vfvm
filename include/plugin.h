5c680680c60e7645a1f88c7fb7b4c26657b27fb2
BenV <165034+benv666@users.noreply.github.com>
2023-05-10 08:51:49 +0200

    Add on_interrupt feature, add salt to on_iteration

--- include/plugin.h
+++ include/plugin.h
@@ -5,11 +5,13 @@
 
 typedef bool (init_cb_t)(const unsigned char * const prefix, uint8_t prefix_len);
 // Must be thread-safe!
-typedef void (iteration_cb_t)(const unsigned char * const addr, size_t addr_len);
+typedef void (interrupt_cb_t)(void);
+typedef void (iteration_cb_t)(const unsigned char * const addr, size_t addr_len, const uint64_t salt);
 typedef void (progress_cb_t)(void);
 
 struct plugin {
 	init_cb_t *on_init;
+	interrupt_cb_t *on_interrupt;
 	iteration_cb_t *on_iteration;
 	progress_cb_t *on_progress;
 };
